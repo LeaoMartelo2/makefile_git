bcd971997e5cc5a26489a2438217fef7621271c0
LeaoMartelo <leaomartelo@gmail.com>
2024-02-11 02:17:57 -0300

    fixed header file having useless functions still

--- src/include/func.h
+++ src/include/func.h
@@ -1,12 +1,6 @@
 #ifndef FUNC_H
 #define FUNC_H
 
-#include <string>
-
-void ConsoleMessage(std::string message, bool displayHeader);
-
-void CoordDebugMessage(std::string message, bool showDebug, int y, int x);
-
 void GenRandomRoom(char map[][81], int rows, int cols);
 
 void clearMap(char map[][81]);
